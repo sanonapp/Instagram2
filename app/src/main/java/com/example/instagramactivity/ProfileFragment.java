da2b22fbfdd6d95b002975e7c8a751e6f12d5f0a
sanonapp <mitelson070@gmail.com>
2022-02-05 08:49:42 -0800

    third commit

---
+++ app/src/main/java/com/example/instagramactivity/ProfileFragment.java
@@ -0,0 +1,16 @@
+package com.example.instagramactivity;
+
+import android.util.Log;
+
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+
+import java.util.List;
+
+import fragments.PostFragment;
+
+public class ProfileFragment extends PostFragment {
+
+}
